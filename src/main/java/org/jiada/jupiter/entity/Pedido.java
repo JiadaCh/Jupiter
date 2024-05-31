ddc89ba71882a49ac4beca135dff80acaaca2cd1
Jiada <124351114+JiadaCh@users.noreply.github.com>
2024-05-31 18:42:41 +0200

    Formato Fecha

--- src/main/java/org/jiada/jupiter/entity/Pedido.java
+++ src/main/java/org/jiada/jupiter/entity/Pedido.java
@@ -6,8 +6,6 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.hibernate.annotations.ColumnDefault;
-import org.springframework.boot.context.properties.bind.DefaultValue;
 
 import java.util.Date;
 
